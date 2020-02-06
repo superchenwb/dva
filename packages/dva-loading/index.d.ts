c294b577510743d4bfa9c299f1630883f8e81c82
chenwenbin <chen1990wb@163.com>
2020-02-06 16:43:44 +0800

    fix:添加dva-loading的types

--- packages/dva-loading/index.d.ts
+++ packages/dva-loading/index.d.ts
@@ -1,5 +1,8 @@
+import { Hooks } from 'dva-core';
 export interface DvaLoadingState {
   global: boolean;
   models: { [type: string]: boolean | undefined };
   effects: { [type: string]: boolean | undefined };
 }
+
+export default function createLoading(): Hooks;
